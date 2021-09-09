6924a7a7530b7b84de64c0c52ed802f3b30ec420
zhijianou <zhijian.ou@mdland.com>
2021-09-09 23:08:53 +0800

    ExcutingAnOrderParam

--- MarsRoverExpedition/modules/expedition/models/Param/ExcutingAnOrderParam.cs
+++ MarsRoverExpedition/modules/expedition/models/Param/ExcutingAnOrderParam.cs
@@ -1,21 +1,26 @@
-﻿namespace MarsRoverExpedition.modules.expedition.models.Param
+﻿using System.ComponentModel;
+
+namespace MarsRoverExpedition.modules.expedition.models.Param
 {
     public class ExcutingAnOrderParam
     {
         /// <summary>
         /// 命令
         /// </summary>
+        [DefaultValue("FFLFFRBBHFFFFF")]
         public string Order { get; set; }
         
         /// <summary>
         /// 着陆方向
         /// 上:0, 右:90, 下:180, 左:270
         /// </summary>
+        [DefaultValue("0")]
         public int Direction { get; set; }
         
         /// <summary>
         /// 着陆位置
         /// </summary>
+        [DefaultValue("H17")]
         public string LandId { get; set; }
     }
 }
